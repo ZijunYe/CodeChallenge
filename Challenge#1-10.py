f69237d20902910f5c1eac14df9d6bfddfef98f8
Joy Ye <zijunye001@gmail.com>
2020-12-09 09:58:36 -0500

    code2 update

--- Challenge#1-10.py
+++ Challenge#1-10.py
@@ -1,8 +1,4 @@
 #1
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-#The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
-
 def multiples_sum(x):
     '''
     (int)->int
@@ -10,6 +6,10 @@ def multiples_sum(x):
     23
     >>> multiples_sum(1000)
     233168
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000
+
     >>> 
     '''
     multiple=[]
@@ -19,3 +19,18 @@ def multiples_sum(x):
     return sum(multiple)
             
         
+#2
+
+def Fibonacci(limit):
+    x=0
+    y=1
+    total=0
+    while y <= limit:
+        tmp=y
+        y=x+y
+        x=tmp   # x,y=y,x+y
+        if y%2==0:
+            total+=y
+            
+    print( total)
+    
